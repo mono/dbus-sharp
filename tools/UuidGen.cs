6632dfef8c5d0656d9973fda835278ec613b332d
Alp Toker <alp@atoker.com>
2006-10-05 04:14:10 +0100

    Add uuidgen.exe tool stub

---
+++ tools/UuidGen.cs
@@ -0,0 +1,13 @@
+// Copyright 2006 Alp Toker <alp@atoker.com>
+// This software is made available under the MIT License
+// See COPYING for details
+
+using System;
+
+public class UuidGen
+{
+	public static void Main (string[] args)
+	{
+		throw new NotImplementedException ("Not yet supprted");
+	}
+}
