b01d8438a7f4d18ee7c093060f5c8017c31f4ec1
Alp Toker <alp@atoker.com>
2007-01-10 08:19:28 +0000

    Add new (currently broken) testcase

---
+++ examples/TestExportInterface.cs
@@ -0,0 +1,107 @@
+// Copyright 2007 Alp Toker <alp@atoker.com>
+// This software is made available under the MIT License
+// See COPYING for details
+
+using System;
+using System.Collections.Generic;
+using NDesk.DBus;
+using org.freedesktop.DBus;
+
+public class ManagedDBusTestExport
+{
+	public static void Main ()
+	{
+		Bus bus = Bus.Session;
+
+		ObjectPath myOpath = new ObjectPath ("/org/ndesk/test");
+		string myNameReq = "org.ndesk.test";
+
+		IDemoObject demo;
+
+		if (bus.NameHasOwner (myNameReq)) {
+			demo = bus.GetObject<IDemoObject> (myNameReq, myOpath);
+		} else {
+			RequestNameReply nameReply = bus.RequestName (myNameReq);
+
+			Console.WriteLine ("nameReply: " + nameReply);
+
+			demo = new DemoObject ();
+			bus.Register (myNameReq, myOpath, demo);
+
+			while (true)
+				bus.Iterate ();
+		}
+
+		Console.WriteLine ();
+		demo.SomeEvent += delegate (string arg1, object arg2, double arg3, MyTuple mt) {Console.WriteLine ("SomeEvent handler: " + arg1 + ", " + arg2 + ", " + arg3 + ", " + mt.A + ", " + mt.B);};
+		demo.SomeEvent += delegate (string arg1, object arg2, double arg3, MyTuple mt) {Console.WriteLine ("SomeEvent handler two: " + arg1 + ", " + arg2 + ", " + arg3 + ", " + mt.A + ", " + mt.B);};
+		demo.FireOffSomeEvent ();
+		//handle the raised signal
+		//bus.Iterate ();
+
+		Console.WriteLine ();
+		demo.SomeEvent += HandleSomeEventA;
+		demo.FireOffSomeEvent ();
+		//handle the raised signal
+		//bus.Iterate ();
+
+		Console.WriteLine ();
+		demo.SomeEvent -= HandleSomeEventA;
+		demo.FireOffSomeEvent ();
+		//handle the raised signal
+		//bus.Iterate ();
+
+		Console.WriteLine ();
+	}
+
+	public static void HandleSomeEventA (string arg1, object arg2, double arg3, MyTuple mt)
+	{
+		Console.WriteLine ("SomeEvent handler A: " + arg1 + ", " + arg2 + ", " + arg3 + ", " + mt.A + ", " + mt.B);
+	}
+
+	public static void HandleSomeEventB (string arg1, object arg2, double arg3, MyTuple mt)
+	{
+		Console.WriteLine ("SomeEvent handler B: " + arg1 + ", " + arg2 + ", " + arg3 + ", " + mt.A + ", " + mt.B);
+	}
+}
+
+[Interface ("org.ndesk.test")]
+public interface IDemoObject
+{
+	event SomeEventHandler SomeEvent;
+	void FireOffSomeEvent ();
+}
+
+public class DemoObject : IDemoObject
+{
+	public event SomeEventHandler SomeEvent;
+
+	public void FireOffSomeEvent ()
+	{
+		Console.WriteLine ("Asked to fire off SomeEvent");
+
+		MyTuple mt;
+		mt.A = "a";
+		mt.B = "b";
+
+		if (SomeEvent != null) {
+			SomeEvent ("some string", 21, 19.84, mt);
+			Console.WriteLine ("Fired off SomeEvent");
+		}
+	}
+}
+
+public enum DemoEnum
+{
+	Foo,
+	Bar,
+}
+
+
+public struct MyTuple
+{
+	public string A;
+	public string B;
+}
+
+public delegate void SomeEventHandler (string arg1, object arg2, double arg3, MyTuple mt);
