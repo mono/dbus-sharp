9089c25d192af057cb5f9b0b8a45e3b905ca6bb3
Jérémie Laval <jeremie.laval@gmail.com>
2011-02-09 15:39:34 +0000

    Remove IL for struct writer and MessageWriter code instead

--- tests/MessageWriterTest.cs
+++ tests/MessageWriterTest.cs
@@ -31,5 +31,27 @@ namespace DBus.Tests
 			for (int i = 0; i < initial.Length; i++)
 				Assert.AreEqual (i + 1, BitConverter.ToInt32 (result, 4 + 4 * i), "#" + i);
 		}
+
+		struct TestStruct
+		{
+			public int bleh;
+			public uint bloup;
+			public float blop;
+		}
+
+		[Test]
+		public void WriteStructTest ()
+		{
+			TestStruct stct = new TestStruct ();
+			stct.bleh = 5;
+			stct.bloup = 3;
+			stct.blop = 5.5f;
+
+			writer.WriteStructure<TestStruct> (stct);
+			byte[] result = writer.ToArray ();
+			Assert.AreEqual (5, BitConverter.ToInt32 (result, 0));
+			Assert.AreEqual ((uint)3, BitConverter.ToUInt32 (result, 4));
+			Assert.AreEqual (5.5f, BitConverter.ToSingle (result, 8));
+		}
 	}
 }
