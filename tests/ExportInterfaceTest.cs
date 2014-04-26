1863311ffdb216b15541b920fef565159b60dec4
Jérémie Laval <jeremie.laval@gmail.com>
2014-04-26 16:44:12 -0400

    [cleanup] Fix previous merge style.

--- tests/ExportInterfaceTest.cs
+++ tests/ExportInterfaceTest.cs
@@ -79,7 +79,7 @@ namespace DBus.Tests
 		[Test]
 		public void GetVariant ()
 		{
-			Assert.IsInstanceOf<byte[]> (test.GetSomeVariant());
+			Assert.IsInstanceOfType (typeof (byte []), test.GetSomeVariant());
 		}
 		
 		/// <summary>
